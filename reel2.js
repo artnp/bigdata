f64dae04c13a075bda45d228a85dce131ef452c1
$artnp <99397593+artnp@users.noreply.github.com>
2025-04-01 10:30:45 +0700

    Update file via API

--- reel2.js
+++ reel2.js
@@ -2,4 +2,5 @@ const videoIds = [
 "ZJQjjBR6PbY?start=47&end=95,ทุกสิ่งทุกอย่างประกอบด้วยอะตอม",
 "ZJQjjBR6PbY?start=3021&end=3111,ยืมอะตอมเพื่อขอกำเนิดเป็นชิ้นส่วน",
 "WvoTfWwWsC4?start=22&end=45,กลไกชีวิตเริ่มต้นมาจากในน้ำ",
+"V08nV7VQeNg?start=43&end=63,ใช้ภาพกำหนดฐานเลขใหญ่",
 ];
