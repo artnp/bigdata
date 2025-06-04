907404e4831177ff38c26105038147aa4c5353c4
$artnp <99397593+artnp@users.noreply.github.com>
2025-06-04 11:06:33 +0700

    Update file via API

--- reel1.js
+++ reel1.js
@@ -1096,4 +1096,5 @@ const videoIds = [
 "YgZWDPdyl7k?start=393&end=474,โรคเบาหวานทำลายหลอดเลือดที่ไต นำพาอาการสู่โรคไตเรื้อรัง",
 "YgZWDPdyl7k?start=484&end=546,ความดันโลหิตสูง ดันเส้นหลอดเลือดไตแตกเสียหาย",
 "YgZWDPdyl7k?start=606&end=673,ยาแก้ปวดกลุ่มNSAIDs (Ibuprofen/Diclofenac/Meloxicam/Naproxen/Indomethacin/Piroxicam) ทำให้หลอดเลือดไตตีบ จนไตขาดเลือด",
+"E_ZYNK96a0c?start=94&end=135,ทุ่นระเบิดรถถัง ทำงานเมื่อโดนน้ำหนักกดเกิน 500กิโลกรัม",
 ];
