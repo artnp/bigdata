696106af5841f348200d488e873e72fb16eb280a
$artnp <99397593+artnp@users.noreply.github.com>
2026-01-08 01:30:06 +0700

    Update file via API

--- reel1.js
+++ reel1.js
@@ -1,4 +1,5 @@
 const videoIds = [
 "pZvaf5D2meg?start=89&end=137,ออร์แกนอยด์หรืออวัยวะจิ๋วคือการเพาะเลี้ยงเซลล์ให้เติบโตเป็นอวัยวะจำลองที่มีหน้าที่การทำงานเหมือนของจริง",
 "KCJBW0tIU1A?start=101&end=132,Larry Fink ซีอีโอของ BlackRock ได้ก้าวขึ้นมาเป็นประธานของ World Economic Forum (WEF) แทนที่ Klaus Schwab",
+"KCJBW0tIU1A?start=459&end=484,DFINITY และ Internet Computer (ICP) ถือเป็นสมาชิกระดับ Platinum ของ CV Labs ซึ่งเป็นระดับสูงสุด",
 ];
