80b5e46ccf035d862bd1286f601ef30586f7925b
$artnp <99397593+artnp@users.noreply.github.com>
2026-01-08 09:24:05 +0700

    Update file via API

--- reel1.js
+++ reel1.js
@@ -3,4 +3,5 @@ const videoIds = [
 "KCJBW0tIU1A?start=101&end=132,Larry Fink ซีอีโอของ BlackRock ได้ก้าวขึ้นมาเป็นประธานของ World Economic Forum (WEF) แทนที่ Klaus Schwab",
 "KCJBW0tIU1A?start=459&end=484,DFINITY และ Internet Computer (ICP) ถือเป็นสมาชิกระดับ Platinum ของ CV Labs ซึ่งเป็นระดับสูงสุด",
 "e75-0M9ID0Q,เข้าเดือยไม้แบบหล่อปูน โดยไม่ต้องตีตะปู",
+"TfmYiR0JPdA,อุปกรณ์ Cylinder Puller หรือบางครั้งเรียกว่า Bell Puller เครื่องมือนี้จัดอยู่ในกลุ่ม Locksmith Specialized Tools ที่ใช้สำหรับดึงไส้กุญแจ ออกมาโดยตรงเมื่อไม่สามารถใช้วิธีสะเดาะกุญแจได้",
 ];
