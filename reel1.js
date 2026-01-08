a097ef1850fb70c2612ce2df2beb1e32164459b5
$artnp <99397593+artnp@users.noreply.github.com>
2026-01-08 09:05:20 +0700

    Update file via API

--- reel1.js
+++ reel1.js
@@ -2,4 +2,5 @@ const videoIds = [
 "pZvaf5D2meg?start=89&end=137,ออร์แกนอยด์หรืออวัยวะจิ๋วคือการเพาะเลี้ยงเซลล์ให้เติบโตเป็นอวัยวะจำลองที่มีหน้าที่การทำงานเหมือนของจริง",
 "KCJBW0tIU1A?start=101&end=132,Larry Fink ซีอีโอของ BlackRock ได้ก้าวขึ้นมาเป็นประธานของ World Economic Forum (WEF) แทนที่ Klaus Schwab",
 "KCJBW0tIU1A?start=459&end=484,DFINITY และ Internet Computer (ICP) ถือเป็นสมาชิกระดับ Platinum ของ CV Labs ซึ่งเป็นระดับสูงสุด",
+"e75-0M9ID0Q,เข้าเดือยไม้แบบหล่อปูน โดยไม่ต้องตีตะปู",
 ];
