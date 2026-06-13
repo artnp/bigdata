7b3d0370694de8aa334c095376ad89f2ffd37ab5
$artnp <99397593+artnp@users.noreply.github.com>
2026-06-13 18:05:18 +0700

    Add clip: eKS8vyNA2ZY to reel6.js

--- reel6.js
+++ reel6.js
@@ -609,4 +609,5 @@ const videoIds = [
 "8e0oX9QYMkk?start=35&end=95,This video features a beautiful cover of the theme from Crouching Tiger, Hidden Dragon performed on the erhu by Eliott Tordo, accompanied by Valentin Catil on the cello . The performance, which takes place in a serene, nature-filled setting, showcases the expressive qualities of both instruments , .",
 "fK9soy_U4ik?start=266&end=326,การที่ธุรกิจสีเทา เช่น ร้านพนันหรือหวยเถื่อน รอดตัวได้เพราะใช้เงินลงทุนต่ำและเป็นที่พึ่งทางใจชั่วคราวสำหรับคนตกงานหรือผู้สูงอายุ",
 "eKS8vyNA2ZY?start=259&end=319,โทนเสียง: เสียงสูงๆ เช่น การจุ๊ปาก หรือดนตรีที่มีโน้ตสูง ดึงดูดแมวได้ดีเพราะคล้ายเสียงสัตว์ขนาดเล็ก",
+"eKS8vyNA2ZY?start=281&end=341,กลิ่นเหงื่อ: เหงื่อมีเกลือและฟีโรโมน ซึ่งเป็นข้อมูลที่แมวสนใจและใช้ทำความรู้จักคุณ",
 ];
